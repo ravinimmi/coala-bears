c240bc42980b7694e1be4d16a13f03eaf1e70fc7
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2016-02-22 21:25:15 +0530

    bears/yaml: Add bear for YAML using yamllint
    Fixes: https://github.com/coala-analyzer/coala-bears/issues/60

---
+++ tests/yaml/YAMLLintBearTest.py
@@ -0,0 +1,32 @@
+import os
+
+from bears.yaml.YAMLLintBear import YAMLLintBear
+from tests.LocalBearTestHelper import verify_local_bear
+
+test_file = """
+---
+receipt:     Oz-Ware Purchase Invoice
+date:        2012-08-06
+customer:
+    first_name:   Dorothy
+    family_name:  Gale
+
+items:
+    - part_no:   A4786
+      descrip:   Water Bucket (Filled)
+      price:     1.47
+      quantity:  4
+""".split("\n")
+
+script_dir = os.path.dirname(__file__)
+config_file = os.path.join(script_dir, 'config.yml')
+
+YAMLLintBear1Test = verify_local_bear(YAMLLintBear,
+                                      valid_files=(),
+                                      invalid_files=(test_file,))
+
+YAMLLintBear2Test = verify_local_bear(YAMLLintBear,
+                                      valid_files=(test_file,),
+                                      invalid_files=(),
+                                      settings={
+                                          'yamllint_config': config_file})
